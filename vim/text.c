6cade0d96093d275cdd3b3b4e5f49f1058cc7ae1
YuFan-Ji <972321520@qq.com>
2024-11-06 20:58:55 +0800

    Linux下vim编辑器命令学习及环境配置

---
+++ vim/text.c
@@ -0,0 +1,23 @@
+include<stdio.h>
+
+int main()
+int main()
+int main()
+int main()
+int main()
+int main()
+int main()
+int main()
+int main()
+int main()
+int main()
+int main()
+int main()
+{
+    printf("hello world, hello jyf!\n"); printf("hello world, hello jyf!\n");
+    printf("hello worLd, hello jyf!\n");
+    printf("hello world, hello jyf!\n");
+    printf("hello world, hello jyf!\n"); printf("hello world, hello jyf!\n");
+
+    return 0;
+}
